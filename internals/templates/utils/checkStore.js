0fb8842c0220451e0f5ccb69369797c5199692db
AceGentile <ventogrigio83@gmail.com>
2019-11-28 08:16:35 +0100

    reduce lodash in bundle size
    use import `lodash/stuff` over `import { stuff } from lodash`
    when importing libraries from lodash

--- internals/templates/utils/checkStore.js
+++ internals/templates/utils/checkStore.js
@@ -1,4 +1,7 @@
-import { conformsTo, isFunction, isObject } from 'lodash';
+import conformsTo from 'lodash/conformsTo';
+import isFunction from 'lodash/isFunction';
+import isObject from 'lodash/isObject';
+
 import invariant from 'invariant';
 
 /**
